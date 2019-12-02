49575c4c2becb0f8f2ad4fb9a661f96c5d793838
Kit Jonathan Te <kjonathante@gmail.com>
2019-12-01 18:22:03 -0800

    Updates

---
+++ precache-manifest.db23bb479576004c3339bf1afb7dd6f7.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "38719925d84941c4079b",
+    "url": "/static/js/app.120e7844.chunk.js"
+  },
+  {
+    "revision": "2bc11b2c0c44eb4067d3",
+    "url": "/static/js/runtime~app.5e92a5d2.js"
+  },
+  {
+    "revision": "778f046fcc00db5a0c39",
+    "url": "/static/js/2.08e8ff0b.chunk.js"
+  },
+  {
+    "revision": "3a2ba31570920eeb9b1d217cabe58315",
+    "url": "/./fonts/AntDesign.ttf"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "/./fonts/Entypo.ttf"
+  },
+  {
+    "revision": "6beba7e6834963f7f171d3bdd075c915",
+    "url": "/./fonts/Feather.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/./fonts/FontAwesome.ttf"
+  },
+  {
+    "revision": "c6aef942e3668158ec29d4adcb2e768f",
+    "url": "/./fonts/FontAwesome5_Brands.ttf"
+  },
+  {
+    "revision": "872545dde71de3842234bf6afe80c4cb",
+    "url": "/./fonts/FontAwesome5_Solid.ttf"
+  },
+  {
+    "revision": "e20945d7c929279ef7a6f1db184a4470",
+    "url": "/./fonts/Foundation.ttf"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "/./fonts/Ionicons.ttf"
+  },
+  {
+    "revision": "5a293a273bee8d740a045d9922b9a9ae",
+    "url": "/./fonts/MaterialCommunityIcons.ttf"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/./fonts/MaterialIcons.ttf"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/./fonts/SimpleLineIcons.ttf"
+  },
+  {
+    "revision": "d0c694b562b2208635f250762cd7fc79",
+    "url": "/serve.json"
+  },
+  {
+    "revision": "7a7bc7ead25db795e58b336f04d2624c",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "5e695e96a003a79f7f97060bf49409a9",
+    "url": "/expo-service-worker.js"
+  },
+  {
+    "revision": "afe7dba443f2a19c8f01c22ea000e38d",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "6847767ce2f5c9cf000bdde4e9b29732",
+    "url": "/index.html"
+  },
+  {
+    "revision": "236a3185132168c5bde9f7606e816c7b",
+    "url": "/static/js/app.120e7844.chunk.js.gz"
+  },
+  {
+    "revision": "6552f7ab963936142a23ec3c4eb8e8ee",
+    "url": "/static/js/runtime~app.5e92a5d2.js.gz"
+  },
+  {
+    "revision": "bd56db0844d50841ee6e0b69eb5e794d",
+    "url": "/expo-service-worker.js.gz"
+  },
+  {
+    "revision": "ea194e6df19e5bcf2cebb4c6662f314e",
+    "url": "/static/js/2.08e8ff0b.chunk.js.gz"
+  }
+];
